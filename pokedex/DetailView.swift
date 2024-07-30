640d221922e05dec390639bab323ab849801aff6
DanielePerrupane <148336607+DanielePerrupane@users.noreply.github.com>
2024-07-30 18:09:27 +0200

    first commit

---
+++ pokedex/DetailView.swift
@@ -0,0 +1,72 @@
+//
+//  DetailView.swift
+//  pokedex
+//
+//  Created by Daniele Perrupane on 30/07/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        
+        ZStack {
+            
+            RoundedRectangle(cornerRadius: 8)
+                .frame(width: 200, height: 200)
+            Text("Pokèmon \nImage")
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.white)
+                .border(Color.black)
+            
+        }
+        VStack(alignment: .center) {
+            
+            HStack {
+                Text("Type 1")
+                    .padding(.vertical,5)
+                    .padding(.horizontal,15)
+                    .background(
+                        RoundedRectangle(cornerRadius: 15.0)
+                            .fill(.clear)
+                            .background(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                            )
+                        
+                    )
+                   
+                Text("Type 2")
+                    .padding(.vertical,5)
+                    .padding(.horizontal,15)
+                    .background(
+                        RoundedRectangle(cornerRadius: 15.0)
+                            .fill(.clear)
+                            .background(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                            )
+                        
+                    )
+                
+                
+            }
+            .padding(.bottom)
+            
+            RoundedRectangle(cornerRadius: 5.0)
+                .frame(width: 100,height: 25)
+                .overlay {
+                    Text("Flavor Text")
+                        .foregroundStyle(.white)
+                }
+            
+        }
+        
+        
+            
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Daniele Perrupane on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
